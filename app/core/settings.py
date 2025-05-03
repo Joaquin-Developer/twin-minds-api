8fee5496a81e1817b4be89996a2e5cbb754ca00b
jparrilla_meli <joaquin.p.olivera@gmail.com>
2025-05-02 21:23:14 -0300

    backend cors origins in .env files

--- app/core/settings.py
+++ app/core/settings.py
@@ -12,8 +12,7 @@ class Settings(BaseSettings):
     DESCRIPTION: str = "Twin Minds Backend Service"
     ENVIRONMENT: str
 
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173",  "http://127.0.0.1:5173"]
-
+    BACKEND_CORS_ORIGINS: List[str]
     USE_LOCAL_STORAGE: bool
     MONGODB_URL: str
 
